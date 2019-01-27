eb904b166c4841adf39147fe52c184c0f67b2b98
Tom <tom@catcobralizard.com>
2019-01-27 15:33:50 -0800

    better destroy droplet script

--- destroy.py
+++ destroy.py
@@ -11,12 +11,22 @@ def destroy_droplet(token):
         'Content-Type': 'application/json'
     }
 
+    droplet_id = get_droplet_id()
     params = {
-        'id': get_droplet_id()
+        'id': droplet_id
     }
 
-    print(f'destroying droplet ...')
-    requests.delete('https://api.digitalocean.com/v2/droplets', headers=headers, params=params)
+    print(f'destroying droplet {droplet_id}...')
+    url = 'https://api.digitalocean.com/v2/droplets'
+    request = requests.delete(url, headers=headers, params=params)
+
+    # see https://github.com/requests/requests/blob/master/requests/status_codes.py
+    # pylint: disable=E1101
+    if request.status_code != requests.codes.no_content:
+        print('Something went wrong. ' + request.json()['message'])
+        return
+
+    print(f'destroyed droplet {droplet_id}.')
 
 if __name__ == '__main__':
     destroy_droplet(retrieve_token())
